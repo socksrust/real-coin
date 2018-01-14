82e987c62848ce1d6b08fc1fe00de78d77bd5d01
jaburcodes <guilhermejabur@outlook.com>
2018-01-14 21:02:28 -0200

    add styled-components to CoinCard

--- components/CoinCard.js
+++ components/CoinCard.js
@@ -1,95 +1,133 @@
 import React from 'react'
 import { StyleSheet, View, Text, Image } from 'react-native'
+import styled from 'styled-components/native'
+
+const Wrapper = styled.View`
+  height: 120;
+  flex-direction: row;
+  background: ${props => props.backgroundColor ? props.backgroundColor : 'white'};
+`
+
+const ImageWrapper = styled.View`
+  flex: 2;
+  justify-content: center;
+  align-items: center;
+`
+
+const ImageInnerWrapper = styled.View`
+  background-color: #ffffff;
+  height: 68;
+  width: 68;
+  border-radius: 50;
+  display: flex;
+  margin-left: 0;
+`
+
+const CoinImage  = styled.Image`
+  width: 68;
+  height: 68;
+`
+/*basic - infos*/
+
+const InfosWrapper = styled.View`
+  flex: 4;
+  flex-direction: row;
+`
+
+const BasicInfosWrapper = styled.View`
+  flex: 1;
+  margin-top: 26;
+  margin-bottom: 16;
+  justify-content: space-between
+`
+
+const DisplayNameText = styled.Text`
+  color: #ffffff;
+  font-size: 12;
+  font-weight: bold;
+`
+const NameWrapper = styled.View`
+  flex-direction: row;
+  margin-top: 3;
+  width: 50;
+  background-color: #000000;
+  border-radius: 50;
+  align-items: center;
+  justify-content: center;
+`
+
+const NameText = styled.Text`
+  color: #ffffff;
+  fontSize: 11;
+`
+const LightText = styled.Text`
+  color: gray;
+  font-size: 10;
+  font-weight: bold;
+`
+
+/*advanced - infos*/
+
+const AdvancedInfosWrapper = styled.View`
+  flex: 1;
+  margin-top: 26;
+  margin-bottom: 26;
+`
+
+const CoinPriceText = styled.Text`
+  color: #ffffff;
+  font-size: 12;
+  font-weight: bold;
+`
+
+const InfoSpan = styled.Text`
+  color: #73F2C8;
+`
+
+const GraphWrapper = styled.View`
+  flex: 1;
+  margin-bottom: 18;
+  justify-content: flex-start;
+  align-items: center;
+`
+
+
 
 export default class CoinCard extends React.Component {
   render () {
     const { image, backgroundColor, currentPrice, name, displayName } = this.props
     return (
-      <View style={[styles.wrapper, {backgroundColor: backgroundColor}]}>
-        <View style={styles.imageWrapper}>
-          <View style={styles.image}>
-            <Image
-              style={{width: 68, height: 68}}
+      <Wrapper backgroundColor={backgroundColor}>
+        <ImageWrapper>
+          <ImageInnerWrapper>
+            <CoinImage
               source={{uri: image}}
             />
-          </View>
-        </View>
-        <View style={styles.infosWrapper}>
-          <View style={styles.basicInfosWrapper}>
+          </ImageInnerWrapper>
+        </ImageWrapper>
+        <InfosWrapper>
+          <BasicInfosWrapper>
             <View>
-              <Text style={styles.coinName}>{displayName}</Text>
-              <View style={{flexDirection: 'row', marginTop: 3}}>
-                <View style={{width: 50, backgroundColor: '#000000', borderRadius: 50, alignItems: 'center', justifyContent: 'center'}}>
-                  <Text style={{color: '#ffffff', fontSize: 11}}>{name}</Text>
-                </View>
-              </View>
+              <DisplayNameText>{displayName}</DisplayNameText>
+              <NameWrapper>
+                <NameText>{name}</NameText>
+              </NameWrapper>
             </View>
             <View>
-              <Text style={styles.lightText}>Created at: 10/06/2019</Text>
+              <LightText>Created at: 10/06/2019</LightText>
             </View>
-          </View>
-          <View style={styles.advancedInfosWrapper}>
-            <View style={styles.advancedInfos}>
-              <Text style={styles.coinName}>Price: <Text style={{color: '#73F2C8'}}>${currentPrice}</Text></Text>
-              <Text style={styles.coinName}>Change: <Text style={{color: '#73F2C8'}}>$29.41%</Text></Text>
+          </BasicInfosWrapper>
+          <AdvancedInfosWrapper>
+            <View>
+              <CoinPriceText>Price: <InfoSpan>${currentPrice}</InfoSpan></CoinPriceText>
+              <CoinPriceText>Change: <InfoSpan>$29.41%</InfoSpan></CoinPriceText>
             </View>
-            <View style={styles.graphWrapper}>
+            <GraphWrapper>
               <Text style={{fontSize: 30, color: '#FFFFFF', marginTop: 15}}>X</Text>
-            </View>
-          </View>
-        </View>
-      </View>
+            </GraphWrapper>
+          </AdvancedInfosWrapper>
+        </InfosWrapper>
+      </Wrapper>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  wrapper: {
-    height: 120,
-    flexDirection: 'row'
-  },
-  imageWrapper: {
-    flex: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  image: {
-    backgroundColor: '#ffffff',
-    height: 68,
-    width: 68,
-    borderRadius: 50,
-    display: 'flex',
-    marginLeft: 0
-  },
-  infosWrapper: {
-    flex: 4,
-    flexDirection: 'row'
-  },
-  basicInfosWrapper: {
-    flex: 1,
-    marginTop: 26,
-    marginBottom: 16,
-    justifyContent: 'space-between'
-  },
-  advancedInfosWrapper: {
-    flex: 1,
-    marginTop: 26,
-    marginBottom: 26
-  },
-  coinName: {
-    color: '#ffffff',
-    fontSize: 12,
-    fontWeight: 'bold'
-  },
-  lightText: {
-    color: 'gray',
-    fontSize: 10,
-    fontWeight: 'bold'
-  },
-  graphWrapper: {
-    flex: 1,
-    marginBottom: 18,
-    justifyContent: 'flex-start',
-    alignItems: 'center'
-  }
-})
